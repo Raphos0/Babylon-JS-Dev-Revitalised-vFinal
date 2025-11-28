bdaac756c8d6d1a588f9aedfbf71bd77b6923168
Raphos0 <raphos2907@outlook.com>
2025-11-28 00:27:41 +0000

    BIG progress on element 4

---
+++ babylonGate/player04/src/interfaces.ts
@@ -0,0 +1,35 @@
+import {
+    Scene,
+    ArcRotateCamera,
+    AssetsManager,
+    Vector3,
+    HemisphericLight,
+    MeshBuilder,
+    Mesh,
+    Camera,
+    Engine,
+    HavokPlugin,
+    PhysicsCharacterController,
+    Quaternion,
+    CharacterSupportedState,
+    KeyboardEventTypes,
+    PhysicsAggregate,
+    PhysicsShapeType,
+    Light,
+    PointLight,
+    ShadowGenerator,
+    StandardMaterial,
+    Color3,
+    Texture,
+  } from "@babylonjs/core";
+
+export interface SceneData {
+    scene: Scene;
+    light?: Light;
+    plight? : PointLight;
+    shadowGenerator?: ShadowGenerator;
+    ground?: PhysicsAggregate;
+    camera?: Camera;
+    box1?:PhysicsAggregate;
+    box2?:PhysicsAggregate;
+}
